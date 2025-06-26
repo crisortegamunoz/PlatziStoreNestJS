27d50ba533204469e73561e7aabcc52ff5c87031
Cristian Ortega <crisortegamunoz@gmail.com>
2025-06-26 16:21:13 -0400

    Creacion entidades faltantes del proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,16 @@ import { ProductsController } from './controllers/products/products.controller';
 import { CategoriesController } from './controllers/categories/categories.controller';
 import { ProductsService } from './services/products/products.service';
 import { CategoriesService } from './services/categories/categories.service';
+import { CustomersService } from './services/customers/customers.service';
+import { BrandsService } from './services/brands/brands.service';
+import { UsersService } from './services/users/users.service';
+import { BrandsController } from './controllers/brands/brands.controller';
+import { CustomersController } from './controllers/customers/customers.controller';
+import { UsersController } from './controllers/users/users.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
-  providers: [AppService, ProductsService, CategoriesService],
+  controllers: [AppController, ProductsController, CategoriesController, BrandsController, CustomersController, UsersController],
+  providers: [AppService, ProductsService, CategoriesService, CustomersService, BrandsService, UsersService],
 })
 export class AppModule {}
